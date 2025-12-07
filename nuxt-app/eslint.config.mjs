fba667503b8dee00593a47e24f57ccac1fce4858
Greg <greg.miller@felixforyou.ca>
2025-12-06 21:15:52 -0400

    Add nuxt app

---
+++ nuxt-app/eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+  rules: {
+    "@stylistic/quotes": "off",
+    "brace-style": "off",
+    "@stylistic/brace-style": "off",
+    "comma-dangle": "off",
+    "@stylistic/comma-dangle": "off",
+    "object-curly-spacing": "off",
+    "@stylistic/object-curly-spacing": "off",
+    "array-bracket-spacing": "off",
+    "@stylistic/array-bracket-spacing": "off",
+    "arrow-parens": "off",
+    "@stylistic/arrow-parens": "off",
+    "space-before-blocks": "off",
+    "@stylistic/space-before-blocks": "off",
+    "space-before-function-paren": "off",
+    "@stylistic/space-before-function-paren": "off",
+    "space-in-parens": "off",
+    "@stylistic/space-in-parens": "off",
+  },
+});
+// Your custom configs here
